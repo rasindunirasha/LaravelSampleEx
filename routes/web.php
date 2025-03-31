33c911eb687a36381d3006946926c38259e7224c
rasindunirashagithub <rasindunirasha7@gmail.com>
2025-03-31 14:48:52 +0530

    use as a class job

--- routes/web.php
+++ routes/web.php
@@ -3,42 +3,51 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
 
+class job{
 
-$jobs = [
-        [
-            'id'  => '1',
-            'title' => 'Director',
-            'salary' => '$30,000'
-        ],
-        [
-            'id'  => '2',
-            'title' => 'Programmer',
-            'salary' => '$40,000'
-        ],
-        [
-            'id'  => '3',
-            'title' => 'Teacher',
-            'salary' => '$50,000'
-        ]
-    ];
+    public static function all():array
+    {
+        return [
+            [
+                'id'  => '1',
+                'title' => 'Director',
+                'salary' => '$30,000'
+            ],
+            [
+                'id'  => '2',
+                'title' => 'Programmer',
+                'salary' => '$40,000'
+            ],
+            [
+                'id'  => '3',
+                'title' => 'Teacher',
+                'salary' => '$50,000'
+            ]
+
+            ];
+        } 
+}
+
+      
+
 
 Route::get('/', function () {
     return view ('home');
 });
 
 
-Route::get('/jobs', function() use($jobs){
+Route::get('/jobs', function() {
     return view('jobs',
-    ['jobs' => $jobs
+    ['jobs' => job::all()
     
      ]);
 });
 
 
-Route::get('/jobs/{id}' ,function ($id) use($jobs){
+Route::get('/jobs/{id}' ,function ($id) {
 
     
-    $job =    Arr::first($jobs, fn($job) => $job['id'] === $id);
+    $job =    Arr::first(job::all(), fn($job) => $job['id'] === $id);
    
     return view ('job',['job' =>$job]);
 });
